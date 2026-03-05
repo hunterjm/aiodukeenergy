b8fe790a2a62ee915f869a5595ec76faeccf453e
crash0verride11 <3526616+crash0verride11@users.noreply.github.com>
2026-03-05 18:46:23 -0500

    feat: safari-extension for macOS users
    * feat: add safari extension built using the Xcode built-in extension converter.
    * Added extension icon
    * Unable to test with Android endpoint.

---
+++ safari-extension/Duke Energy OAuth Helper/Duke Energy OAuth Helper/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Duke Energy OAuth Helper
+//
+//  Created by Raymond Lanser on 2/18/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
